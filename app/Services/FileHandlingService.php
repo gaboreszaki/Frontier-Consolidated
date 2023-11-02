1954859e03c2bf0c5679f6a2b75b78e8b7e9aeb1
Mr. Gabor Eszaki <24547662+gaboreszaki@users.noreply.github.com>
2023-11-02 16:49:43 +0100

    Dynamic image handling refactor and extension - added modularity - added storeOriginal fn - added processImageByType fn
    Signed-off-by: Mr. Gabor Eszaki <24547662+gaboreszaki@users.noreply.github.com>

--- app/Services/FileHandlingService.php
+++ app/Services/FileHandlingService.php
@@ -1,49 +1,70 @@
 <?php
-
-namespace App\Services;
-
-use Illuminate\Support\Facades\Storage;
-use Intervention\Image\Facades\Image;
-
-class FileHandlingService{
     
-
-    function store($type, $file){
-        if ($file){
+    namespace App\Services;
+    
+    use Illuminate\Support\Facades\Storage;
+    use Intervention\Image\Facades\Image;
+    
+    class FileHandlingService
+    {
+        
+        public function storeOriginal($file)
+        {
+            if (!$file) {
+                return null;
+            }
             
             $name = $file->hashName();
             //store Original:
             Storage::put("public/original", $file);
-            
-        switch ($type){
-            case 'thumbnail':
+            return  $name;
+        }
+        public function processImageByType($type, $fileName)
+        {
 
-                $width = 300;
-                $height = null;
-                $quality = 80;
-                $folderName = "thumbnails";
-                
-                }
             
-            $this->processFile($name, $folderName, $width, $height, $quality );
+
             
-            return $name;
-        }else{
-            return null;
+            /////////
+            /// Process files with mode selected:
+            switch ($type) {
+                case 'thumbnail':
+                    
+                    $width = 300;
+                    $height = null;
+                    $quality = 80;
+                    $folderName = "thumbnails";
+                    $this->processFile($fileName, $folderName, $width, $height, $quality);
+                    
+                    return $fileName;
+                    
+                    
+                case 'card':
+                    $width = null;
+                    $height = 300;
+                    $quality = 80;
+                    $folderName = "card";
+                    $this->processFile($fileName, $folderName, $width, $height, $quality);
+                    
+                    return $fileName;
+            }
+            
+return null;
+        }
+        
+        private function processFile(
+            $fileName,
+            $saveFolder,
+            ?int $width ,
+            ?int $height,
+            $quality
+        ) {
+            $img = Image::make('storage/original/'.$fileName);
+            $img->resize($width, $height, function ($constraint) {
+                $constraint->aspectRatio();
+            });
+            
+            $img->save(public_path("storage/$saveFolder/$fileName"), $quality);
         }
         
     }
-    
-    private function processFile($fileName,  $saveFolder, $width=null, $height=null, $quality){
-        
-        
-        $img = Image::make('storage/original/'.$fileName);
-        $img->resize($width, $height, function ($constraint) {
-            $constraint->aspectRatio();
-        });
-        
-        $img->save(public_path("storage/$saveFolder/$fileName"),$quality );
-
-    }
-
-}
