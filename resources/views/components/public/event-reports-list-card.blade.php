82bbe56097cbbd87b607d6d9822b4324ae4885b6
Mr. Gabor Eszaki <24547662+gaboreszaki@users.noreply.github.com>
2023-11-02 18:42:18 +0100

    Dynamic image handling refactor and extension - fixed bug with empty image - changed card display settings
    Signed-off-by: Mr. Gabor Eszaki <24547662+gaboreszaki@users.noreply.github.com>

---
+++ resources/views/components/public/event-reports-list-card.blade.php
@@ -0,0 +1,12 @@
+<div class="card" >
+@if($report->op_cover_img)
+    <img src="{{asset('storage/card/'.$report->op_cover_img)}}" class="card-img-top" alt="{{$report->title}}">
+    @else
+    <x-tools.image-placeholder/>
+    @endif
+        <div class="card-body">
+        <h5 class="card-title">{{$report->title}}</h5>
+        <small class="text-secondary">{{$report->op_date}}</small>
+        <a href="#" class="btn btn-primary " disabled="disabled">Read report </a>
+    </div>
+</div>
