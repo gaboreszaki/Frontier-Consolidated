c9715d26403c3c9c5c61972cc82433c6c8b7695f
Mr. Gabor Eszaki <24547662+gaboreszaki@users.noreply.github.com>
2023-12-28 12:02:14 +0100

    Fix - public community page now using storage
    Signed-off-by: Mr. Gabor Eszaki <24547662+gaboreszaki@users.noreply.github.com>

--- resources/views/components/public/slice-item-display.blade.php
+++ resources/views/components/public/slice-item-display.blade.php
@@ -3,13 +3,13 @@
     {{-- Todo: create a more elegant solution --}}
     <figure class="image">
 
-        <img class="img-fluid " src="{{url('images/temp/'.$item->background_image)}}" alt="background-test">
+        <img class="img-fluid " src="{{Storage::url('originals/'.$item->background_image)}}" alt="background-test">
 
 
-            <div class="figcaption {{$item->layout}} ">
-                <div class="indicator">
-                    <h2>{{$item->title}}</h2>
-                    <div>{{$item->content}}</div>
+        <div class="figcaption {{$item->layout}} ">
+            <div class="indicator">
+                <h2>{{$item->title}}</h2>
+                <div>{!!  $item->content !!}</div>
                 </div>
             </div>
 
