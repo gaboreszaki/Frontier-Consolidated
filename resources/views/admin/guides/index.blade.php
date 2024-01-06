0df1eb96e11729bedbfc9b86f0f00aeab4bc1e94
Mr. Gabor Eszaki <24547662+gaboreszaki@users.noreply.github.com>
2024-01-06 13:36:09 +0100

    WIP: Guides - Added Livewire to Guides Index - Added basic search to guides
    Signed-off-by: Mr. Gabor Eszaki <24547662+gaboreszaki@users.noreply.github.com>

--- resources/views/admin/guides/index.blade.php
+++ resources/views/admin/guides/index.blade.php
@@ -1,3 +1,4 @@
+<!--suppress ALL -->
 <x-layouts.admin>
     <div class="row">
         <div class="col-12">
@@ -10,23 +11,10 @@
     <hr>
     <div class="row">
         <div class="col-12">
-            @foreach ($guides as $item)
-                <div class="card shadow mb-3">
-                    <div class="card-body ">
-                        <div class="card-title">
-                            <div class="float-end">
-                                <a href="{{route('admin.guides.show', ['guide' => $item->id])}}" class="btn btn-primary btn-sm"><i class="bi bi-eye"></i></a>
-                                <a href="{{route('admin.guides.edit', ['guide' => $item->id])}}" class="btn btn-success btn-sm"><i class="bi bi-pencil"></i></a>
-                            </div>
-                            <h5>{{$item->title}}</h5>
-                        </div>
-                        <hr>
-                        <div class="">
-                            {!! mb_substr($item->description, 0, 200) !!}...
-                        </div>
-                    </div>
-                </div>
-            @endforeach
+            <livewire:guides-index  />
+
+
+
         </div>
     </div>
 </x-layouts.admin>
